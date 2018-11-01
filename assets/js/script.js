ba9d97511f1415f4c7a66f69f390842cbb016710
Pearl <gansuvd777@gmail.com>
2018-11-01 00:17:24 -0500

    adding firebase on contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,48 @@ $("#factButton").on("click", function() {
 // This array holds all of Mariah's quotes!
 var mariahQuoteArray = ["'Learn to be quiet enough to hear the genuine within yourself so that you can hear it in others'", "'Don't feel entitled to anything you didn't sweat and struggle for'", "'If things are too easy, life is a whole lot less interesting'", "You're not obligated to win. You're obligated to keep trying to do the best you can every day."]
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDUB-tglFxCYsEiSmErxQPKjY72G8ILTUE",
+    authDomain: "fir-bbd58.firebaseapp.com",
+    databaseURL: "https://fir-bbd58.firebaseio.com",
+    projectId: "fir-bbd58",
+    storageBucket: "fir-bbd58.appspot.com",
+    messagingSenderId: "1012219301290"
+  };
+  
+  firebase.initializeApp(config);
 
+var database = firebase.database();
+
+$("#submit").on("click", function(event) {
+  console.log("you clicked submit");
+  event.preventDefault();
+  var name = $("#name")
+    .val()
+    .trim();
+  var email = $("#email")
+    .val()
+    .trim();
+  var message = $("#message")
+    .val()
+    .trim();
+  var newContact = {
+    name: name,
+    email: email,
+    message: message
+  };
+  console.log(newContact);
+
+  database.ref().push(newContact);
+  console.log(newContact.name);
+  console.log(newContact.email);
+  console.log(newContact.message);
+
+  $("#name").val("");
+  $("#email").val("");
+  $("#message").val("");
+});
 
 // $(document).ready(function(){
 //     $(".img-thumbnail").mouserover(function(){
